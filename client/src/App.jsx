c4792191b1131e110ddaab79dc9a18ace3277796
romnickk <155442557+romnickk@users.noreply.github.com>
2024-01-03 16:48:31 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
  
   const handleSubmit = (e) =>{
     e.preventDefault();
-    axios.post('reg-api-six.vercel.app/register',  {name, username, password})
+    axios.post('https://reg-api-six.vercel.app//register',  {name, username, password})
     .then(result => console.log(result))
     .catch(err => console.log(err))
   }
