996f009a2bae4ec27d5da31ddcf0285fcf7007fe
romnickk <155442557+romnickk@users.noreply.github.com>
2024-01-03 16:51:47 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
+        origin: ["https://reg-client-mu.vercel.app/"],
         methods:["POST","GET"],
         credentials:true
     }
